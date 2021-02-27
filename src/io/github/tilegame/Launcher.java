7c4c062a4b4c56abe65480c30ae2f055f5e8beed
jaron <jaronwilson2024@gmail.com>
2021-02-26 19:31:13 -0500

    Yes

--- src/io/github/tilegame/Launcher.java
+++ src/io/github/tilegame/Launcher.java
@@ -2,6 +2,7 @@ package io.github.tilegame;
 
 public class Launcher {
 
+    // Jarons farts stink
     public static void main(String[] args) {
        Game game = new Game("/res/worlds/world1.txt", 640,360);
        game.start();
