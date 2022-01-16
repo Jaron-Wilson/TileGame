e37f6af4ca0a791ddc1a35777a0e85235268c649
Jaron-Wilson <JaronWilson2024@gmail.com>
2022-01-16 15:55:57 -0500

    Fixed loading of images

--- src/io/github/tilegame/Launcher.java
+++ src/io/github/tilegame/Launcher.java
@@ -2,9 +2,8 @@ package io.github.tilegame;
 
 public class Launcher {
 
-    // Jarons farts stink
     public static void main(String[] args) {
-       Game game = new Game("/res/worlds/world1.txt", 640,360);
+       Game game = new Game("world1", 640,360);
        game.start();
     }
 
